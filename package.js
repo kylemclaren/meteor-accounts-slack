c23ea5d77f484ff4e662d4e257ba9a1748033fc6
Kyle McLaren <kyle.mclaren1@gmail.com>
2015-08-25 23:26:29 -0500

    update version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Login service for Slack accounts",
-  version: "1.0.7",
+  version: "1.0.8",
   git: "https://github.com/kylemclaren/meteor-accounts-slack.git",
   name: "kylemclaren:accounts-slack"
 });
